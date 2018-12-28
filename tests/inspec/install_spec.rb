cb9b855b4cf1e19f8b3c34b6a1bb667d9003dbf0
diodonfrost <diodon.frost@diodonfrost.me>
2018-12-28 11:08:33 +0100

    add inspec tests

---
+++ tests/inspec/install_spec.rb
@@ -0,0 +1,23 @@
+# Test ntp daemon install
+
+ntp_package = "ntp"
+ntp_service = "ntp"
+
+control 'install-01' do
+  impact 1.0
+  title 'Ntp package'
+  desc 'Ntp daemon package should be install'
+  describe package(ntp_package) do
+    it { should be_installed }
+  end
+end
+
+control 'install-02' do
+  impact 1.0
+  title 'Ntp service'
+  desc 'Ntp service should be enabled and started'
+  describe service(ntp_service) do
+    it { should be_enabled }
+    it { should be_running }
+  end
+end
